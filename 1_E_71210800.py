af824f2b75a5b05d39cd8f6158218d2028ac946b
Evan <stefaron.budhi@ti.ukdw.ac.id>
2021-11-11 23:59:04 +0700

    71210800_Soal 1

---
+++ 1_E_71210800.py
@@ -0,0 +1,30 @@
+#bagian a
+print("Test case 1 :")
+
+#input
+atas = int(input("Masukkan alas atas :"))
+bawah = int(input("Masukkan alas bawah :"))
+tinggi = int(input("Masukkan tinggi :"))
+
+#proses
+luas = 0.5 * (atas + bawah) * tinggi
+
+#cetak
+print ("Luas trapesium adalah :", luas)
+
+print()
+print()
+
+#bagian b
+print("Test case 2 :")
+
+#input
+atas = int(input("Masukkan alas atas :"))
+bawah = int(input("Masukkan alas bawah :"))
+tinggi = int(input("Masukkan tinggi :"))
+
+#proses
+luas = 0.5 * (atas + bawah) * tinggi
+
+#cetak
+print ("Luas trapesium adalah :", luas)
