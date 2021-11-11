f81eb8aa4c53bab71adab2a0382a03133bb85627
Evan <stefaron.budhi@ti.ukdw.ac.id>
2021-11-12 00:05:57 +0700

    71210800_Soal 3

---
+++ 3_E_71210800.py
@@ -0,0 +1,26 @@
+print("Test case 1:")
+
+#input
+belanja1 = input("Masukkan daftar belanja 1:")
+belanja2 = input("Masukkan daftar belanja 2:")
+tambah1 = input("Tambahkan data ke daftar belanja 1:")
+tambah2 = input("Tambahkan data ke daftar belanja 2:")
+
+#proses
+print("Daftar belanja 1 adalah", belanja1, "," , tambah1)
+print("Daftar belanja 2 adalah", belanja2, "," , tambah2)
+
+print()
+print()
+
+print("Test case 2:")
+
+#input
+belanja1 = input("Masukkan daftar belanja 1:")
+belanja2 = input("Masukkan daftar belanja 2:")
+tambah1 = input("Tambahkan data ke daftar belanja 1:")
+tambah2 = input("Tambahkan data ke daftar belanja 2:")
+
+#proses
+print("Daftar belanja 1 adalah", belanja1, "," ,tambah1)
+print("Daftar belanja 2 adalah", belanja2, "," ,tambah2)
